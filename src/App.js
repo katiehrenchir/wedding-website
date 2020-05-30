c165fe27a445b99e86e0d341119fb0cb27b8e2c8
katiehrenchir <katie.hrenchir@gmail.com>
2020-05-29 21:32:32 -0500

    Add Wedding Website button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class WeddingButton extends React.Component {
+  render() {
+    return (
+      <div className="kp_container">
+        <Button outline size="lg" color="info" onClick={() => alert('welcome to our wedding website')}>
+          Wedding Website
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default App;
+// ========================================
+
+ReactDOM.render(
+<div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}}>
+    <WeddingButton />
+  </div>,
+  document.getElementById('root')
+);
+
+export default WeddingButton;
